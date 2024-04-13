18438bbab874162da9264b43bd8ff0113d3530ae
kroy2022 <kevinroy2015@gmail.com>
2024-04-12 22:53:02 -0500

    Started frontend and backend

---
+++ backend/venv/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template, url_for
+from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect 
+import requests
+
+app = Flask(__name__)
+CORS(app)
+csrf = CSRFProtect(app)
+
+@app.route('/twitter/info', methods=["POST"])
+@csrf.exempt
+def get_twitter_info():
+    try:
+        link = request.form.get("link")
+        
+        print(link)
+        return jsonify({
+            "status": "Valid",
+            "link": link
+        })
+    
+    except Exception as e:
+        return jsonify({
+            "status": "Invalid",
+            "error": str(e)
+        })
